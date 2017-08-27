0f2a1e70f493cad856d38c7ec26507169ee5ee26
Luis Alberto Pena Nunez <luis.pena.nunez.developer@gmail.com>
2017-08-27 17:24:48 -0500

    Changed .NET Profile

--- .nuget/build.sh
+++ .nuget/build.sh
@@ -1,3 +1,3 @@
-nuget pack "./../XForms/Me/Me.csproj" -Version 1.0.0 -OutputDirectory "./build/" -Properties "Configuration=Release;Platform=AnyCPU"
+nuget pack "./../XForms/Me/Me.csproj" -Version 1.0.1 -OutputDirectory "./app/" -Properties "Configuration=Release;Platform=AnyCPU"
 
-nuget push ./build/*.nupkg $1 -Source https://www.nuget.org/api/v2/package
+nuget push ./app/*.nupkg $1 -Source https://www.nuget.org/api/v2/package
